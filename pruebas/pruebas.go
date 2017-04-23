315b079dd014d689a48cf33d04de48f011a08900
jorger3 <jromussi@gmail.com>
2017-04-23 01:11:09 -0300

    for numericos

--- pruebas/pruebas.go
+++ pruebas/pruebas.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola Umedy!!")
+	// fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+
+	for i := 0; i < 100; i++ {
+		fmt.Printf("%d - %b - %x - %q \n", i, i, i, i)
+
+	}
 }
